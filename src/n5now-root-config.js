9e4750ea60761d092f4991be3404bf3ea4a8634f
Juan Pablo Caguasango Enriquez <juan.enriquez@masiv.com>
2023-08-28 19:49:50 -0500

    feat: :sparkles: Finish build orchestrator

--- src/n5now-root-config.js
+++ src/n5now-root-config.js
@@ -1,18 +1,15 @@
 import { registerApplication, start } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
-
 registerApplication({
   name: "@n5now/app-rick-and-morty",
   app: () => System.import("@n5now/app-rick-and-morty"),
   activeWhen: ["/rick-and-morty"],
+});
+
+registerApplication({
+  name: "@n5now/app-simpsons",
+  app: () => System.import("@n5now/app-simpsons"),
+  activeWhen: ["/simpsons"],
 });
 
 start({
